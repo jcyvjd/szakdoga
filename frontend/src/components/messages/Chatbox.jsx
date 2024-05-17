38e9df279fa5de612fd1998affcabe172bf920fc
kissadam21 <kiss21adam@gmail.com>
2024-05-17 23:10:09 +0200

    Sidepanel hides

--- frontend/src/components/messages/Chatbox.jsx
+++ frontend/src/components/messages/Chatbox.jsx
@@ -4,9 +4,6 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { useAuthContext } from "../../context/AuthContext";
 import { useRoomContext } from "../../context/RoomContext";
-import { Collapse, Ripple, initTWE } from "tw-elements";
-
-initTWE({ Collapse, Ripple });
 
 const Chatbox = () => {
   const { authUser } = useAuthContext();
@@ -16,19 +13,6 @@ const Chatbox = () => {
     : null;
 
     return (
-      // <div className="rounded-lg h-screen flex flex-col" style={{ maxHeight: '80vh' }}>
-      //   <button
-      //     className=" md:hidden inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-      //     type="button"
-      //     data-twe-collapse-init
-      //     data-twe-ripple-init
-      //     data-twe-ripple-color="light"
-      //     data-twe-target="#collapseExample"
-      //     aria-expanded="false"
-      //     aria-controls="collapseExample"
-      //   >
-      //     Chat
-      //   </button> 1 sorral lentro id="collapseExample" data-twe-collapse-item
         <div className="flex flex-col bg-base-200 text-base-content" >
           <div className="bg-neutral text-neutral-content px-4 py-2 rounded-lg">
             <span className="">Chat:</span>{" "}
